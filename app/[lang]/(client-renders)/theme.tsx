40b9303bf8512b4a039f31ccc951fdebc846f169
Jake Walklate <jakewalklate@protonmail.com>
2025-11-04 00:12:45 +1100

    ui

--- app/[lang]/(client-renders)/theme.tsx
+++ app/[lang]/(client-renders)/theme.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import {
   getCookie,
   setCookie,
@@ -22,9 +23,17 @@ export default function ThemeToggle() {
   };
 
   return (
-    <button onClick={() => setTheme()} data-slot="button" className="cursor-pointer focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive inline-flex shrink-0 items-center justify-center gap-2 rounded-md text-sm font-medium whitespace-nowrap transition-all outline-none focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&amp;_svg]:pointer-events-none [&amp;_svg]:shrink-0 [&amp;_svg:not([class*='size-'])]:size-4 bg-background hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 border shadow-xs size-9" type="button" id="radix-_R_6qnpfiv5ubsnpnb_" aria-haspopup="menu" aria-expanded="false" data-state="closed">
-      <SunMoon />
-      <span className="sr-only">Theme</span>
-    </button>
+    <TooltipProvider delay={100}>
+      <Tooltip>
+        <TooltipTrigger onClick={() => setTheme()} className="cursor-pointer focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive inline-flex shrink-0 items-center justify-center gap-2 rounded-md text-sm font-medium whitespace-nowrap transition-all outline-none focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&amp;_svg]:pointer-events-none [&amp;_svg]:shrink-0 [&amp;_svg:not([class*='size-'])]:size-4 bg-background hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 border shadow-xs size-9" type="button" id="radix-_R_6qnpfiv5ubsnpnb_" aria-haspopup="menu" aria-expanded="false" data-state="closed">
+          <SunMoon />
+          <span className="sr-only">Theme</span>
+        </TooltipTrigger>
+        <TooltipContent side="bottom" className="max-xl:hidden">
+          <p>Change theme</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+
   );
 }
