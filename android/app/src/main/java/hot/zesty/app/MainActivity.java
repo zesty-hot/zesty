e2f020069cec984ae7e049eb67403504ac52ea3d
Jake Walklate <jakewalklate@protonmail.com>
2025-11-26 14:02:13 +1100

    capacitor + remove stripe

---
+++ android/app/src/main/java/hot/zesty/app/MainActivity.java
@@ -0,0 +1,5 @@
+package hot.zesty.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'hot.zesty.app',
-  appName: 'zesty',
-  webDir: 'public'
+  appName: 'Zesty',
+  server: {
+    url: 'https://zesty.hot'
+  }
 };
 
 export default config;
